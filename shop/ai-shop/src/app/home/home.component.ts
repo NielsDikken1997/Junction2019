a66934c058398ec2cf374afa942977ace17d927d
mkufel <m.kufel@hotmail.com>
2019-11-16 21:32:18 +0200

    Dirty commit for JJ

--- shop/ai-shop/src/app/home/home.component.ts
+++ shop/ai-shop/src/app/home/home.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import * as tf from '@tensorflow/tfjs';
+// import deepModel from '../../assets/model.json';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +8,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+    model: tf.LayersModel;
+    constructor() { }
 
   ngOnInit() {
+      this.loadModel();
+    // const model = tf.loadLayersModel(deepModel);
+    // const merged = [["TotalTime",	"PictureClicks",	"PictureTime",	"PictureDwelling", "SummaryClicks",	"SummaryTime", "SummaryDwelling", "DescriptionClicks",	"DescriptionTime",	"DescriptionDwelling",	"ReviewClicks",	"ReviewTime", "ReviewDwelling"], [76844, 4,	17495,	4222,	1,	24315,	8835,	4,	18848,	17426,	3,	646,	646]];
+    // console.log(merged);
+    console.log(this.model);
+
+  }
+  async loadModel() {
+    this.model = await tf.loadLayersModel('../../assets/model.json');
   }
 
 }
