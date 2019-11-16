f34ed9bcef918bacf5ef1187c93d614bf384db0a
mkufel <m.kufel@hotmail.com>
2019-11-16 23:56:42 +0200

    Model with mockup data

--- shop/ai-shop/src/app/home/home.component.ts
+++ shop/ai-shop/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import * as tf from '@tensorflow/tfjs';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
+  model: tf.LayersModel;
   constructor() { }
 
   ngOnInit() {
+    this.loadModel();
+  }
+  async loadModel() {
+    this.model = await tf.loadLayersModel('/assets/model.json');
+    console.log(this.model);
+    const tensor = tf.tensor([60422,	11,	12630,	9829,	4,	9778,	2452,	6,	18888,	2072,	0,	13000,	9000], [1, 13],'int32');
+
+    const pred = this.model.predict(tensor).dataSync();
+    // console.log(this.model.predict(tensor).dataSync());
+    const willReturn = pred.indexOf(Math.min(...pred)) === 1;
+    console.log(willReturn);
   }
 
 }
+
