68b7600af80fcb53947b195a316e9af7a392ef7f
Lukas Brückner <me@luksurious.me>
2019-11-16 21:20:57 +0200

    basic tracking in localstorage

--- shop/ai-shop/src/app/app.component.ts
+++ shop/ai-shop/src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { TrackingService } from './tracking.service';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter }                from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ai-shop';
+
+  constructor(private trackingService: TrackingService, private router: Router) {}
+
+  ngOnInit() {
+
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe(() => {
+        this.trackingService.start();
+      });
+  }
 }
---
+++ shop/ai-shop/src/app/tracking.service.ts
@@ -0,0 +1,184 @@
+import { Injectable } from '@angular/core';
+
+
+@Injectable({
+    providedIn: 'root',
+})
+
+export class TrackingService {
+    private interval: any;
+
+    private aoiVisible: any = {};
+    private aoiEntries: any = {};
+
+    private aoiHovered: any = {};
+    private aoiHoveredEntries: any = {};
+
+    private aoiInteracted: any = {};
+
+    private id: number;
+
+    private checkInterval: number = 1000;
+
+    start() {
+        this.id = Date.now();
+
+        if (!window.localStorage.getItem('aois')) {
+            let baseObject = {};
+            window.localStorage.setItem('aois', JSON.stringify(baseObject));
+        }
+        
+        this.checkVisibleAois();
+        this.checkHoveredAois();
+        this.trackAoiClicks();
+    }
+
+    checkVisibleAois() {
+        let options = {
+            root: document.querySelector('#content-container'),
+            rootMargin: '0px',
+            threshold: 0.5
+        }
+        let callback = (entries, observer) => { 
+            entries.forEach(entry => {
+                let aoiName = entry.target.getAttribute('data-aoi');
+
+                if (entry.intersectionRatio >= 0.5) {
+                    // moved in
+                    this.aoiEntries[aoiName] = Date.now();    
+                } else {
+                    // moved out
+                    if (this.aoiEntries[aoiName]) {
+                        let timeInside = Date.now() - this.aoiEntries[aoiName];
+
+                        this.aoiVisible[aoiName].push({
+                            start: this.aoiEntries[aoiName],
+                            duration: timeInside
+                        });
+
+                        this.save();
+                    }
+                }
+            });
+        };
+          
+        let observer = new IntersectionObserver(callback, options);
+
+        document.querySelectorAll('[data-aoi]').forEach(el => {
+            observer.observe(el);
+
+            let aoiName = el.getAttribute('data-aoi');
+
+            this.aoiVisible[aoiName] = [];
+            this.aoiEntries[aoiName] = null;
+        });
+    }
+
+    checkHoveredAois() {
+        document.querySelectorAll('[data-aoi]').forEach(el => {
+            let aoiName = el.getAttribute('data-aoi');
+
+            this.aoiHovered[aoiName] = [];
+            this.aoiHoveredEntries[aoiName] = null;
+        });
+
+        document.addEventListener('mousemove', this.createMouseMoveHandler());
+    }
+
+    createMouseMoveHandler() {
+        let lastCheck = 0;
+
+        return (ev) => {
+            let now = Date.now();
+            if (now - lastCheck < this.checkInterval) {
+                return;
+            }
+            lastCheck = now;
+
+            this.checkMousePosition(ev.x, ev.y);
+            this.save();
+        };
+    }
+
+    checkMousePosition(x, y) {
+        let found = null;
+        document.querySelectorAll('[data-aoi]').forEach(el => {
+            if (found) {
+                return;
+            }
+            
+            let aoiName = el.getAttribute('data-aoi');
+            let elPos = this.getElementsPosition(el);
+
+            if (x >= elPos.left && x <= elPos.right && y >= elPos.top && y <= elPos.bottom) {
+                found = aoiName;
+
+                console.log("Mouse in");
+
+                if (!this.aoiHoveredEntries[aoiName]) {
+                    this.aoiHoveredEntries[aoiName] = Date.now();
+                }
+            }
+        });
+
+        for (let aoi in this.aoiHoveredEntries) {
+            if (aoi == found) {
+                return;
+            }
+
+            if (this.aoiHoveredEntries[aoi]) {
+                this.aoiHovered[aoi].push({
+                    start: this.aoiHoveredEntries[aoi],
+                    duration: Date.now() - this.aoiHoveredEntries[aoi] - this.checkInterval/2
+                });
+                this.aoiHoveredEntries[aoi] = null;
+            }
+        }
+
+    }
+
+    getElementsPosition(el) {
+        let rect = el.getBoundingClientRect();
+        let win = el.ownerDocument.defaultView;
+
+        return {
+            top: rect.top + win.pageYOffset,
+            left: rect.left + win.pageXOffset,
+            bottom: rect.top + win.pageYOffset + rect.height,
+            right: rect.left + win.pageXOffset + rect.width
+        };
+    }
+
+    trackAoiClicks() {
+        document.querySelectorAll('[data-aoi]').forEach(el => {
+            let aoiName = el.getAttribute('data-aoi');
+
+            this.aoiInteracted[aoiName] = 0;
+
+            el.querySelectorAll('[data-aoi-click]').forEach(clickEl => {
+                clickEl.addEventListener('click', ev => {
+                    this.aoiInteracted[aoiName]++;
+                    this.save()
+                });
+            });
+        });
+
+    }
+
+    save() {
+        let aois = JSON.parse(window.localStorage.getItem('aois'));
+        if (!aois[this.id]) {
+            aois[this.id] = {
+                visible: {},
+                hovered: {},
+                clicked: {}
+            };
+        }
+
+        aois[this.id].visible = this.aoiVisible;
+        aois[this.id].hovered = this.aoiHovered;
+        aois[this.id].clicked = this.aoiInteracted;
+
+        window.localStorage.setItem('aois', JSON.stringify(aois));
+    }
+}
