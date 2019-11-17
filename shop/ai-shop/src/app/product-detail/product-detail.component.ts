9dc8f76f1fe3e8866a8ba046c8241322000ce9f8
Vincent <giardina.v@gmail.com>
2019-11-17 09:51:08 +0200

    yolo wors

--- shop/ai-shop/src/app/product-detail/product-detail.component.ts
+++ shop/ai-shop/src/app/product-detail/product-detail.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductDetailComponent implements OnInit {
   private checkout = false;
+  private productDetails = 1;
   
   constructor() { }
 
